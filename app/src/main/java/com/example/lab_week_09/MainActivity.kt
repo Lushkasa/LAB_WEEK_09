54d7263370719df67e44bf5d87736f901d492fcc
Jennifer Jeo <jennifer.jeo@student.umn.ac.id>
2025-11-06 21:39:55 +0700

    Commit No. 3 – UI Element and Theme

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -26,6 +26,9 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
+import com.example.lab_week_09.ui.theme.OnBackgroundItemText
+import com.example.lab_week_09.ui.theme.OnBackgroundTitleText
+import com.example.lab_week_09.ui.theme.PrimaryTextButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -96,36 +99,35 @@ fun HomeContent(
             modifier = Modifier.padding(16.dp).fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
             ) {
-            Text(text = stringResource(
-                id = R.string.enter_item)
-            )
-            //Here, we use TextField to display a text input field
-            TextField(
-                //Set the value of the input field
-                value = inputField.name,
-                //Set the keyboard type of the input field
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Text
-                ),
-                        onValueChange = {
-                    onInputValueChange(it)
-                }
-            )
-            Button(onClick = {
-                onButtonClick()
-            }) {
-                Text(text = stringResource(
-                    id = R.string.button_click)
+                OnBackgroundTitleText(text = stringResource(
+                    id = R.string.enter_item)
                 )
+
+                //Here, we use TextField to display a text input field
+                TextField(
+                    //Set the value of the input field
+                    value = inputField.name,
+                    //Set the keyboard type of the input field
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text
+                    ),
+                            onValueChange = {
+                        onInputValueChange(it)
+                    }
+                )
+                PrimaryTextButton(text = stringResource(
+                    id = R.string.button_click)
+                ) {
+                    onButtonClick()
+                }
             }
-        }
         }
         items(listData) { item ->
             Column(
                 modifier = Modifier.padding(vertical = 4.dp).fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = item.name)
+                OnBackgroundItemText(text = item.name)
             }
         }
     }
